f5f165c333e2c23bcd62ac370469e7399e901737
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-08-25 22:14:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { Router, Routes, Route, Link } from "react-router-dom";
 import Home from './Home.js';
 import Contact from './Contact.js';
 import Menu from './Menu.js';
+import CinmaticVideo from './CinmaticVideo/CinmaticVideo.js';
+import Cinmaticv from './CinmaticVideo/Cinmaticv';
+import AboutUs from './AboutUs/Index';
+import Faq from './Home/Faq.js';
+import Footer from './Home/Footer.js';
 
 
 function App() {
@@ -13,9 +18,14 @@ function App() {
            
     
        <Routes>
-        <Route path="/demo-rect-app" exact element={<Home/>} />
+        <Route path="/testfinalcutproduction" exact element={<Home/>} />
         <Route path="/contact" element={<Contact/>} />
+        <Route path="/cinmatic-video" element={<CinmaticVideo/>} />
+        <Route path="Cinmaticv/:id" element={<Cinmaticv />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
+
+      <Footer/>
 
 
     </div>
